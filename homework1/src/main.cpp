11b011d5b2153741ddf6cc9f3119fff0d26e41e1
CAP1Sup <xmanpiper@gmail.com>
2023-08-29 12:13:37 -0400

    Homework 1 should be done

--- homework1/src/main.cpp
+++ homework1/src/main.cpp
@@ -1,16 +1,28 @@
-/**
- * Demonstration of controlling two servos simultanesouly. See documentation for
- * servo.h/.cpp for more details.
- *
- * */
+// Written by: Christian Piper
+// Date: 8/29/2023
 
 #include <Arduino.h>
-#include <Chassis.h>
 #include <wpi-32u4-lib.h>
 
 // Declare a chassis object with nominal dimensions
 // In practice, adjust the parameters: wheel diam, encoder counts, wheel track
 Chassis chassis(7.0, 1440, 14.9);
+
+#define MOVE_SPEED 1   // Inches per second
+#define TURN_SPEED 15  // Degrees per second
+#define IN_TO_CM 2.54
+
+/**
+ * @brief Drive in a polygon
+ * @param sides Number of sides
+ * @param length Length of each side in inches
+ */
+void polygon(int sides, float length) {
+  for (int sideIndex = 0; sideIndex < sides; sideIndex++) {
+    chassis.driveFor(length * IN_TO_CM, MOVE_SPEED * IN_TO_CM);
+    chassis.turnFor(360.0 / (float)sides, TURN_SPEED);
+  }
+}
 
 /*
  * This is the standard setup function that is called when the board is rebooted
@@ -23,6 +35,9 @@ void setup() {
 
   // these can be undone for the student to adjust
   //chassis.setMotorPIDcoeffs(5, 0.5);
+
+  // Drive in a polygon
+  polygon(3, 12);
 }
 
 /*
