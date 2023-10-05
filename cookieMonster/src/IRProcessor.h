ea8e4e76013cd5882a0d51b3aadd4c0e520741b6
Christian Piper <xmanpiper@gmail.com>
2023-10-04 22:12:01 -0400

    Added Cookie Monster project
    Rewrote IR decoder into IR processor, which will act on codes after it receives them

---
+++ cookieMonster/src/IRProcessor.h
@@ -0,0 +1,115 @@
+#pragma once
+
+#include <Arduino.h>
+
+// Remote constants
+#define REMOTE_ADDRESS_BYTE0 0x00
+#define REMOTE_ADDRESS_BYTE1 0xBF
+
+#define REMOTE_VOL_MINUS 0x00
+#define REMOTE_PLAY_PAUSE 0x01
+#define REMOTE_VOL_PLUS 0x02
+
+#define REMOTE_SETUP 0x04
+#define REMOTE_UP 0x05
+#define REMOTE_STOP_MODE 0x06
+
+#define REMOTE_LEFT 0x08
+#define REMOTE_ENTER_SAVE 0x09
+#define REMOTE_RIGHT 0x0A
+
+#define REMOTE_0 0x0C
+#define REMOTE_DOWN 0x0D
+#define REMOTE_BACK 0x0E
+
+#define REMOTE_1 0x10
+#define REMOTE_2 0x11
+#define REMOTE_3 0x12
+
+#define REMOTE_4 0x14
+#define REMOTE_5 0x15
+#define REMOTE_6 0x16
+
+#define REMOTE_7 0x18
+#define REMOTE_8 0x19
+#define REMOTE_9 0x1A
+
+/** \class IRProcessor
+ * A class to interpret IR remotes with NEC encoding.
+ *
+ * NEC encoding sends four bytes: [device ID, ~divice ID, key code, ~key code]
+ *
+ * Sending the inverse allow for easy error checking (and reduces saturation in
+ * the receiver).
+ *
+ * Codes are send in little endian; this library reverses upon reception, so the
+ * first bit received is in the LSB of currCode. That means that the key code is
+ * found in bits [23..16] of currCode
+ *
+ * https://techdocs.altium.com/display/FPGA/NEC+Infrared+Transmission+Protocol
+ *
+ * This does not interpret the codes into which key was pressed. That needs to
+ * be mapped on a remote by remote basis.
+ */
+class IRProcessor {
+ private:
+  uint8_t pin = -1;
+
+  enum IR_STATE {
+    IR_READY,     // idle, returns to this state after you request a code
+    IR_PREAMBLE,  // received the start burst, waiting for first bit
+    IR_REPEAT,    // received repeat code (part of NEC protocol); last code will
+                  // be returned
+    IR_ACTIVE,    // have some bits, but not yet complete
+    IR_COMPLETE,  // a valid code has been received
+    IR_ERROR      // an error occurred; won't return a valid code
+  };
+
+  IR_STATE state = IR_READY;  // a simple state machine for managing reception
+
+  volatile uint32_t lastReceiveTime =
+      0;  // not really used -- could be used to sunset codes
+
+  volatile uint32_t currCode = 0;  // the most recently received valid code
+  volatile uint8_t index = 0;      // for tracking which bit we're on
+
+  volatile uint32_t fallingEdge = 0;
+  volatile uint32_t risingEdge = 0;
+
+  volatile uint32_t lastRisingEdge =
+      0;  // used for tracking spacing between rising edges, i.e., bit value
+
+  // Function to run when a code is received
+  void (*processCodeFn)(void) = NULL;
+
+ public:
+  IRProcessor(uint8_t p) : pin(p) {}
+  void init(void (*processCode)(void));  // call this in the setup()
+  void handleIRsensor(void);             // ISR
+
+  uint32_t getCode(void)  // returns the most recent valid code; returns zero if
+                          // there was an error
+  {
+    if (state == IR_COMPLETE || state == IR_REPEAT) {
+      state = IR_READY;
+      return currCode;
+    } else
+      return 0;
+  }
+
+  int16_t getKeyCode(
+      bool acceptRepeat =
+          false)  // returns the most recent key code; returns -1 on error (not
+                  // sure if 0 can be a code or not!!!)
+  {
+    if (state == IR_COMPLETE || (acceptRepeat == true && state == IR_REPEAT)) {
+      state = IR_READY;
+      return (currCode >> 16) & 0x0ff;
+    } else
+      return -1;
+  }
+};
+
+extern IRProcessor irProcessor;
+
+// #include "IRProcessor.cpp"
---
+++ cookieMonster/src/main.cpp
@@ -0,0 +1,318 @@
+#include <Arduino.h>
+#include <BlueMotor.h>
+#include <Chassis.h>
+#include <Rangefinder.h>
+#include <Romi32U4.h>
+#include <RotaryGripper.h>
+
+#include "IRProcessor.h"
+
+// Op Settings
+// Chassis
+#define FORWARD_SPEED 12   // in/s
+#define TURN_SPEED 90      // deg/s
+#define SEARCH_EFFORT 100  // Motor power, 0-300ish
+
+// Line following
+#define BLACK_THRESHOLD 500  // White: ~40, Black: ~800
+#define LINE_FOLLOW_P 0.1    // deg/s per difference in sensor values
+
+// 4 Bar
+#define MANUAL_MOVE_EFFORT 50           // % of max effort
+#define ENCODER_SAMPLING_TIME 100       // ms
+#define STAGING_PLATFORM_ANGLE -19.1    // deg
+#define HOUSE_45_DEG_PANEL_ANGLE 41.78  // deg
+#define HOUSE_25_DEG_PANEL_ANGLE 75.14  // deg
+#define CLEARANCE_ANGLE 70              // deg
+
+// Conversions
+#define INCHES_TO_CM 2.54
+
+// Pin settings
+#define IR_PIN 2
+#define US_TRIG_PIN 3
+#define US_ECHO_PIN 7
+#define L_LINE_FOLLOW_PIN A2
+#define R_LINE_FOLLOW_PIN A3
+#define GRIPPER_FEEDBACK_PIN A4
+
+// Create the objects
+Chassis chassis;
+IRProcessor irProcessor(IR_PIN);
+Rangefinder rangefinder(US_ECHO_PIN, US_TRIG_PIN);
+RotaryGripper gripper(A4);
+BlueMotor motor(0, 1);
+Romi32U4ButtonA buttonA;
+Romi32U4ButtonC buttonC;
+
+// Type definitions
+// Direction inverts the sign of the movement
+typedef enum { LEFT = 1, RIGHT = -1 } TURN_DIR;
+
+// Variables
+int16_t irCode = -1;
+int lVal, rVal;
+
+// Function prototypes
+int followUntilCross(TURN_DIR searchDir);
+void followUntilCount(TURN_DIR searchDir, int encoderCount);
+void followUntilDist(TURN_DIR searchDir, float distance);
+void turnOnCross(TURN_DIR turnDir);
+void processIRPress();
+
+// Convenience function
+void waitForButtonA() {
+  while (!buttonA.isPressed())
+    ;
+}
+
+void setup() {
+  // Setup code here
+
+  // Initialize the chassis
+  // ! MUST BE DONE FOR BLUE MOTOR TO WORK
+  chassis.init();
+  chassis.idle();
+
+  // Setup IR decoder
+  irProcessor.init(processIRPress);
+
+  // Setup the ultrasonic sensor
+  rangefinder.init();
+
+  // Initialize the gripper
+  gripper.init();
+
+  // Initialize the motor
+  motor.init();
+
+  // Turn off the motor
+  motor.setEffort(0);
+
+  // Set the motor's current angle
+  // Should always be staging block
+  motor.setAngle(STAGING_PLATFORM_ANGLE);
+
+  // Set the motor's PID constants
+  motor.setKp(15);
+  motor.setKi(0);
+  motor.setKd(2500);
+
+  // Initialize serial and wait for connection
+  Serial.begin(9600);
+  while (!Serial) {
+    delay(10);
+  }
+
+  // Open the gripper
+  gripper.setDesiredState(OPEN);
+}
+
+void loop() {
+  // Print the IR code
+  Serial.println(irCode);
+
+  // Run the whatever the code says
+  if (irCode == REMOTE_UP) {
+    motor.setEffort(MANUAL_MOVE_EFFORT);
+  } else if (irCode == REMOTE_DOWN) {
+    motor.setEffort(-MANUAL_MOVE_EFFORT);
+  } else {
+    motor.setEffort(0);
+  }
+}
+
+/**
+ * @brief Follows the line until the cross is found
+ *
+ * @param searchDir The direction to search for the line
+ * @return int The encoder count when the cross is found
+ */
+int followUntilCross(TURN_DIR searchDir) {
+  // White: ~40
+  // Black: ~800
+
+  // We're not on the line yet
+  bool onLine = false;
+
+  // Loop until we find the cross
+  while (true) {
+    // Get the values from the reflectance sensors
+    // analogRead() calls are expensive
+    lVal = analogRead(L_LINE_FOLLOW_PIN);
+    rVal = analogRead(R_LINE_FOLLOW_PIN);
+
+    if (onLine) {
+      // We're on the line
+      if (lVal > BLACK_THRESHOLD && rVal > BLACK_THRESHOLD) {
+        // We're at the cross
+        chassis.idle();
+
+        // Break the loop and return the encoder count
+        // Add a constant to account for the distance between the line sensor
+        // and the center of the robot
+        return chassis.getLeftEncoderCount(true) + 250;
+
+      } else {
+        // Calculate the difference between the two line sensors
+        int difference =
+            analogRead(L_LINE_FOLLOW_PIN) - analogRead(R_LINE_FOLLOW_PIN);
+        chassis.setTwist(FORWARD_SPEED * INCHES_TO_CM,
+                         LINE_FOLLOW_P * difference);
+      }
+    } else {
+      // We're off the line, turn until we find it
+      chassis.setMotorEfforts(searchDir * -SEARCH_EFFORT,
+                              searchDir * SEARCH_EFFORT);
+
+      // Check if we're on the line
+      if (lVal > BLACK_THRESHOLD || rVal > BLACK_THRESHOLD) {
+        onLine = true;
+        // Reset the left encoder's value
+        chassis.getLeftEncoderCount(true);
+      }
+    }
+
+    // Limit the update rate to allow motors time to move
+    delay(10);
+  }
+}
+
+/**
+ * @brief Follows the line until the given encoder count is reached
+ *
+ * @param searchDir The direction to search for the line
+ * @param encoderCount The encoder count to stop at
+ */
+void followUntilCount(TURN_DIR searchDir, int encoderCount) {
+  // White: ~40
+  // Black: ~800
+
+  // We're not on the line yet
+  bool onLine = false;
+
+  // Loop until we find the cross
+  while (true) {
+    // Get the values from the reflectance sensors
+    // analogRead() calls are expensive
+    lVal = analogRead(L_LINE_FOLLOW_PIN);
+    rVal = analogRead(R_LINE_FOLLOW_PIN);
+
+    if (onLine) {
+      // We're on the line
+      if (chassis.getLeftEncoderCount() >= encoderCount) {
+        // We went the distance we needed to
+        chassis.idle();
+
+        // Break the loop
+        return;
+      } else {
+        // Calculate the difference between the two line sensors
+        int difference =
+            analogRead(L_LINE_FOLLOW_PIN) - analogRead(R_LINE_FOLLOW_PIN);
+        chassis.setTwist(FORWARD_SPEED * INCHES_TO_CM,
+                         LINE_FOLLOW_P * difference);
+      }
+    } else {
+      // We're off the line, turn until we find it
+      chassis.setMotorEfforts(searchDir * -SEARCH_EFFORT,
+                              searchDir * SEARCH_EFFORT);
+
+      // Check if we're on the line
+      if (lVal > BLACK_THRESHOLD || rVal > BLACK_THRESHOLD) {
+        onLine = true;
+        // Reset the left encoder's value
+        chassis.getLeftEncoderCount(true);
+      }
+    }
+
+    // Limit the update rate to allow motors to move
+    delay(10);
+  }
+}
+
+/**
+ * @brief Follows the line until the given minimum distance on the ultrasonic
+ * sensor is reached
+ *
+ * @param searchDir The direction to search for the line
+ * @param distance The minimum distance to stop at (in inches)
+ */
+void followUntilDist(TURN_DIR searchDir, float distance) {
+  // White: ~40
+  // Black: ~800
+
+  // We're not on the line yet
+  bool onLine = false;
+
+  // Loop until we find the cross
+  while (true) {
+    // Get the values from the reflectance sensors
+    // analogRead() calls are expensive
+    lVal = analogRead(L_LINE_FOLLOW_PIN);
+    rVal = analogRead(R_LINE_FOLLOW_PIN);
+
+    if (onLine) {
+      // We're on the line
+      if (rangefinder.getDistance() <= distance * INCHES_TO_CM) {
+        // We went the distance we needed to
+        chassis.idle();
+
+        // Break the loop
+        return;
+      } else {
+        // Calculate the difference between the two line sensors
+        int difference =
+            analogRead(L_LINE_FOLLOW_PIN) - analogRead(R_LINE_FOLLOW_PIN);
+        chassis.setTwist(FORWARD_SPEED * INCHES_TO_CM,
+                         LINE_FOLLOW_P * difference);
+      }
+    } else {
+      // We're off the line, turn until we find it
+      chassis.setMotorEfforts(searchDir * -SEARCH_EFFORT,
+                              searchDir * SEARCH_EFFORT);
+
+      // Check if we're on the line
+      if (lVal > BLACK_THRESHOLD || rVal > BLACK_THRESHOLD) {
+        onLine = true;
+        // Reset the left encoder's value
+        chassis.getLeftEncoderCount(true);
+      }
+    }
+
+    // Limit the update rate to allow motors to move
+    delay(10);
+  }
+}
+
+/**
+ * @brief Moves the robot forward over the cross and turns to the given
+ *
+ * @param turnDir The direction to turn
+ */
+void turnOnCross(TURN_DIR turnDir) {
+  // Move forward over the cross
+  chassis.driveFor(3 * INCHES_TO_CM, FORWARD_SPEED * INCHES_TO_CM, true);
+
+  // Turn to the given direction
+  chassis.turnFor(turnDir * 70, TURN_SPEED, true);
+}
+
+/**
+ * @brief Processes the IR press
+ *
+ */
+void processIRPress() {
+  // Get the key code
+  int16_t keyCode = irProcessor.getKeyCode();
+
+  /*
+    // Check if the code is valid
+    if (keyCode == -1) {
+      // Invalid code
+      return;
+    }*/
+
+  // TODO: Process the code
+  irCode = keyCode;
+}
